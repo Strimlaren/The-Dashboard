824702dcfd7ace610184c7b9cf1be6717b107cc4
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2023-12-06 01:15:40 +0100

    added input field and JS to enable user to use keywords for randomizing background

--- javascript/bg_module.js
+++ javascript/bg_module.js
@@ -1,11 +1,15 @@
 const random_bg_btn = document.getElementById("randomize-backdrop");
 const body = document.querySelector("body");
+const user_keyword = document.getElementById("backdrop-keywords-input");
 random_bg_btn.addEventListener("click", get_new_bg);
 
 async function get_new_bg() {
+  console.log(user_keyword.value);
+  const query = user_keyword.value !== "" ? user_keyword.value : "space";
   const response = await fetch(
-    "https://api.unsplash.com/photos/random?client_id=aPdLF6wNfbJvJugS5A36u58GJrYp08rkIOAwf0fonEo&query=space"
+    `https://api.unsplash.com/photos/random?client_id=aPdLF6wNfbJvJugS5A36u58GJrYp08rkIOAwf0fonEo&query=${query}`
   );
+
   if (response.ok) {
     const data = await response.json();
 
