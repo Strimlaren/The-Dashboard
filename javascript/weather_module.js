209a958262fbeeb37effb0cb1bbdb994ac6d817b
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2023-12-08 19:19:59 +0100

    weather_module coming along nicely

--- javascript/weather_module.js
+++ javascript/weather_module.js
@@ -14,7 +14,11 @@ async function fetch_weather(lat, lon) {
 
   if (weather_data.ok) {
     const data = await weather_data.json();
-    console.log(data);
+
+    const week_data = extract_week(data);
+    create_weather_cards(week_data);
+
+    console.log(week_data);
 
     // const temp_today = ;
     // const weather_description_today = ;
@@ -27,4 +31,38 @@ async function fetch_weather(lat, lon) {
     // const weather_code_daf = ;
   }
   if (!weather_data.ok) console.log("API ERROR!");
+}
+
+function create_weather_cards(array) {
+  array.forEach((day) => {
+    const div1 = new_element("div", "", "forecast-card");
+    const img = new_element("img", "", "weather-img");
+    img.alt = "weather icon";
+    const div2 = new_element("div", "", "forecast-text-content");
+    const h4 = new_element("h4", "GET WHAT DAY IT IS HERE"); // TODO
+    const div3 = new_element("div", "", "data-div");
+    const div4 = new_element("div", "", "data-obj");
+    const div5 = new_element("div", "", "data-obj");
+  });
+}
+
+// Returns a new element of specified type and any number of classes
+function new_element(element_type, content, ...element_classes) {
+  const new_element = document.createElement(`${element_type}`);
+  if (content !== "") new_element.innerText = content;
+  for (let i = 0; i < element_classes.length; i++) {
+    new_element.classList.add(element_classes[i]);
+  }
+  return new_element;
+}
+
+function extract_week(data) {
+  const today = data.list[0];
+  const filtered = [];
+
+  data.list.forEach((dataset) => {
+    if (dataset.dt_txt.includes("12:00")) filtered.push(dataset);
+  });
+  filtered.unshift(today);
+  return filtered;
 }
