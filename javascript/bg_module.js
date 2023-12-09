13393c2f2baf2a1fd42c2ae66cb2d5ed2e05c86a
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2023-12-09 02:32:39 +0100

    fixed background image load when opening page the first time

--- javascript/bg_module.js
+++ javascript/bg_module.js
@@ -3,10 +3,13 @@ const body = document.querySelector("body");
 const user_keyword = document.getElementById("backdrop-keywords-input");
 random_bg_btn.addEventListener("click", get_new_bg);
 
-body.style.backgroundImage = `url("${localStorage.getObj("bgUrl")}")`;
+if (!localStorage.getItem("bgUrl"))
+  body.style.backgroundImage =
+    "https://images.unsplash.com/photo-1630839437035-dac17da580d0?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MzczMTN8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDIwNDUyNTh8&ixlib=rb-4.0.3&q=85";
+else body.style.backgroundImage = `url("${localStorage.getObj("bgUrl")}")`;
 
 async function get_new_bg() {
-  const query = user_keyword.value !== "" ? user_keyword.value : "space";
+  const query = user_keyword.value !== "" ? user_keyword.value : "planet";
 
   const response = await fetch(
     `https://api.unsplash.com/photos/random?client_id=aPdLF6wNfbJvJugS5A36u58GJrYp08rkIOAwf0fonEo&query=${query}`
